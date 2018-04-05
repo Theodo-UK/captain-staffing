2ae9cdc40cd7781a3ca5e9c33c716458b251e863
Jean-Rémi Beaudoin <jeanremi.beaudoin@gmail.com>
2018-04-05 17:33:18 +0100

    Update config

--- src/configs/config.dev.js
+++ src/configs/config.dev.js
@@ -1,7 +1,7 @@
 export default {
-  clientId: '448544502761-7r0d93qdl2rl15s0hhsek8tuiikt0tl5.apps.googleusercontent.com',
+  clientId: '934429149693-lups6u0odnmgr44sp37ndnua39p9bohh.apps.googleusercontent.com',
   scope: 'https://www.googleapis.com/auth/spreadsheets',
-  spreadsheetId: '1NLisLo69DDGc8SXEwOuCCX9X24kKoTsnAM5p82tyx6Y',
+  spreadsheetId: '1SRpAFPoIB8LIAdxFXO36nkDju1bXdon8TJ8ZN7NP6ao',
   trelloBoardId: '55e4146d6855749323241a48',
   trelloListId: '55e4148cc1d7e44b58d0561f',
 }
