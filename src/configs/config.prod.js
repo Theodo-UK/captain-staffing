57ad02ce4ef09f9093efcc10099e8a25889a8f34
Jonathan Wagner <jonathanw@theodo.co.uk>
2023-09-15 12:28:26 +0200

    update client_id to new Captain staffing GCP project

--- src/configs/config.prod.js
+++ src/configs/config.prod.js
@@ -1,5 +1,6 @@
 export default {
-  clientId: '934429149693-lups6u0odnmgr44sp37ndnua39p9bohh.apps.googleusercontent.com',
-  scope: 'https://www.googleapis.com/auth/spreadsheets',
-  spreadsheetId: '1tSvNVwSP080fkQwG7drmQ4c2lPYNWUMZ1_STaWQXXsM',
+  clientId:
+    "1074312790731-krtvpaq8km1h94gspgpqt7o6v8u2ss4g.apps.googleusercontent.com",
+  scope: "https://www.googleapis.com/auth/spreadsheets",
+  spreadsheetId: "1tSvNVwSP080fkQwG7drmQ4c2lPYNWUMZ1_STaWQXXsM",
 }
