cbc5b303a8ec2e3a33a85b910d743560eb90eee5
CallumHemsley <bythepenguin@gmail.com>
2023-12-21 16:47:08 +0000

    feat: basic authResult handling of access token with local storage

--- src/components/CaptainGoogle.jsx
+++ src/components/CaptainGoogle.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useGoogleLogin } from "@react-oauth/google";
 import { load } from "../helpers/spreadsheet";
 
 export const CaptainGoogle = ({ onSuccess, onFailure, onLoad }) => {
+  const storedAuthResult = JSON.parse(localStorage.getItem("authResult"));
+
   const login = useGoogleLogin({
     onSuccess: (authResult) => {
       handleAuth(authResult);
     },
+    auto_select: true,
     scope: import.meta.env.VITE_GOOGLE_SCOPE,
   });
 
   const handleAuth = (authResult) => {
     if (authResult && !authResult.error) {
+      localStorage.setItem("authResult", JSON.stringify(authResult));
+
       onSuccess();
       load(onLoad, authResult);
     } else {
@@ -19,10 +24,19 @@ export const CaptainGoogle = ({ onSuccess, onFailure, onLoad }) => {
     }
   };
 
+  useEffect(() => {
+    if (storedAuthResult) {
+      onSuccess();
+      load(onLoad, storedAuthResult);
+    }
+  }, [onSuccess, onLoad, storedAuthResult]);
+
   return (
-    <button onClick={() => login()} className="btn">
-      Connect with Google
-    </button>
+    <>
+      <button onClick={() => login()} className="btn">
+        Connect with Google
+      </button>
+    </>
   );
 };
 
