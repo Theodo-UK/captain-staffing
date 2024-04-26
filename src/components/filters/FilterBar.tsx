268c51df8019ebf27debe6cbd81d23fb6436ab71
Jonathan Wagner <jonathanw@theodo.co.uk>
2024-04-26 18:22:36 +0200

    feat: replace old company filter with the new multi select

--- src/components/filters/FilterBar.tsx
+++ src/components/filters/FilterBar.tsx
@@ -1,40 +1,7 @@
 import DropdownTreeSelect from "react-dropdown-tree-select";
 // @ts-expect-error file is not typed yet
 import { getPositionForFilter } from "../../helpers/formatter";
-
-const commonFilterStyles = {
-  padding: "6px",
-  marginRight: "10px",
-  cursor: "pointer",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  borderRadius: "4px",
-  border: "2px solid #004262",
-  fontSize: "14px"
-};
-
-const customFilterStyle = {
-  ...commonFilterStyles,
-  backgroundColor: "#33A5FF",
-  color: "white",
-  border: "none"
-};
-
-const companySelectedFilterStyle = {
-  ...commonFilterStyles,
-  border: "2px solid #004262",
-  backgroundColor: "#004262",
-  color: "white"
-};
-
-const companyUnselectedFilterStyle = {
-  ...commonFilterStyles,
-  border: "2px solid #004262",
-  backgroundColor: "white",
-  color: "#004262",
-  opacity: "0.8"
-};
+import { FilterCompanies } from "./FilterCompanies";
 
 interface FilterBarProps {
   positionsState: { [positionName: string]: boolean };
@@ -46,54 +13,25 @@ interface FilterBarProps {
   toggleNoneActive: () => void;
 }
 
-type Company = {
-  name: string;
-  isSelected: boolean;
-};
-
 export const FilterBar: React.FC<FilterBarProps> = ({
-  positionsState,
+  positionsState = {},
   positionLastClicked,
   positionsSelectorOnChange,
-  companiesState,
+  companiesState = {},
   toggleCompanyFilter,
-  toggleAllActive,
+  // toggleAllActive,
   toggleNoneActive
-}) => {
-  const companies: Company[] = Object.entries(companiesState).map(
-    ([companyName, isSelected]) => ({ name: companyName, isSelected })
-  );
-
-  return (
-    <>
-      <DropdownTreeSelect
-        className="positionDropdown"
-        data={getPositionForFilter(positionsState, positionLastClicked)}
-        onChange={positionsSelectorOnChange}
-      />
-      <div className="filter-container">
-        {companies.map(({ name, isSelected }) => (
-          <button
-            key={name}
-            style={
-              isSelected
-                ? companySelectedFilterStyle
-                : companyUnselectedFilterStyle
-            }
-            onClick={() => {
-              toggleCompanyFilter(name);
-            }}
-          >
-            {name}
-          </button>
-        ))}
-        <button style={customFilterStyle} onClick={toggleAllActive}>
-          Toggle All
-        </button>
-        <button style={customFilterStyle} onClick={toggleNoneActive}>
-          Toggle None
-        </button>
-      </div>
-    </>
-  );
-};
+}) => (
+  <div className="flex items-center space-x-4">
+    <DropdownTreeSelect
+      className="positionDropdown"
+      data={getPositionForFilter(positionsState, positionLastClicked)}
+      onChange={positionsSelectorOnChange}
+    />
+    <FilterCompanies
+      toggleNoneActive={toggleNoneActive}
+      toggleCompanyFilter={toggleCompanyFilter}
+      companiesState={companiesState}
+    />
+  </div>
+);
---
+++ src/components/filters/FilterCompanies.tsx
@@ -0,0 +1,56 @@
+import { MultiSelect } from "@/design-system/ui/MultiSelect";
+
+type Company = {
+  name: string;
+  isSelected: boolean;
+};
+
+interface FilterCompaniesProps {
+  toggleNoneActive: () => void;
+  toggleCompanyFilter: (value: string) => void;
+  companiesState: { [companyName: string]: boolean };
+}
+
+export function FilterCompanies({
+  toggleNoneActive,
+  toggleCompanyFilter,
+  companiesState
+}: FilterCompaniesProps) {
+  const companies: Company[] = Object.entries(companiesState).map(
+    ([companyName, isSelected]) => ({
+      name: companyName,
+      isSelected
+    })
+  );
+  const sortedCompanies = sortCompanies(companies).map((c) => ({
+    label: c.name,
+    value: c.name,
+    isSelected: c.isSelected
+  }));
+
+  return (
+    <MultiSelect
+      clearFilter={toggleNoneActive}
+      toggleOption={toggleCompanyFilter}
+      title="Company"
+      options={sortedCompanies}
+    />
+  );
+}
+
+const sortCompanies = (companies: Company[]): Company[] => {
+  let sortedCompanies = [...companies].sort((a, b) =>
+    a.name.localeCompare(b.name)
+  );
+
+  // Put M33 and "NO BU" at the end of the list
+  const END_OF_LIST_COMPANY_NAMES = ["M33", "NO BU"];
+  const endOfListCompanies = sortedCompanies.filter((c) =>
+    END_OF_LIST_COMPANY_NAMES.includes(c.name)
+  );
+  sortedCompanies = sortedCompanies
+    .filter((c) => !END_OF_LIST_COMPANY_NAMES.includes(c.name))
+    .concat(endOfListCompanies);
+
+  return sortedCompanies;
+};
