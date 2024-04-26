c9ce3068f5dc19a483d5c90fc0bf8381b5e0be2b
Jonathan Wagner <jonathanw@theodo.co.uk>
2024-04-26 13:29:03 +0200

    chore: init shadcn design system

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +15,11 @@ export default defineConfig({
     target: "es2020",
     commonjsOptions: {
       include: []
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
     }
   }
 });
