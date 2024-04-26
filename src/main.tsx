5f515ecfee5dcf639f702903246be8802457c80b
Jonathan Wagner <jonathanw@theodo.co.uk>
2024-04-26 14:25:55 +0200

    fix: tailwind style imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,7 @@ import ReactDOM from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 // @ts-expect-error This will be fixed when migrating the rest of the app to TS
 import App from "./App.jsx";
-import "./styles/index.css";
-
-import "./styles/component/alert.css";
-import "./styles/component/app.css";
-import "./styles/component/projects.css";
-import "./styles/component/staffingTable.css";
-
-import "./styles/generic/btn.css";
-import "./styles/generic/loader.css";
+import "./styles/main.css";
 
 const isProd = import.meta.env.PROD;
 const GOOGLE_CLIENT_ID = isProd ? import.meta.env.VITE_GOOGLE_CLIENT_ID : import.meta.env.VITE_GOOGLE_CLIENT_ID_DEV;
